69af850b16bb7d9b1fb8370dd12698181abe9811
Rodrigo Renck <renckrodrigo030@gmail.com>
2022-11-11 16:09:42 -0300

    First commit

---
+++ src/test/EspacoPortoTest.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class EspacoPortoTest {
+
+    @Test
+    public void test() {
+        Assertions.assertEquals(1, 1);
+    }
+}
