bc99765d2a743912668ce9dc33d9b1e4022f145a
Rodrigo Renck <renckrodrigo030@gmail.com>
2022-11-16 15:47:18 -0300

    Implementacao do diagrama de classes

--- src/model/EspacoPorto.java
+++ src/model/EspacoPorto.java
@@ -1,5 +1,22 @@
 package model;
 
+import java.util.List;
+
 public class EspacoPorto {
-    //implementar...
+    private int numero;
+    private String nome;
+    private double coordX;
+    private double coordY;
+    private double coordZ;
+    private List<Espaconave> listaEspaconaves;
+    private List<Transporte> listaTransportes;
+
+    public EspacoPorto(int numero, String nome, double coordX, double coordY, double coordZ) {
+        this.numero = numero;
+        this.nome = nome;
+        this.coordX = coordX;
+        this.coordY = coordY;
+        this.coordZ = coordZ;
+    }
+
 }
--- src/model/Transporte.java
+++ src/model/Transporte.java
@@ -1,5 +1,19 @@
 package model;
 
+import java.util.List;
+
 public abstract class Transporte {
-    //implementar...
+    private int identificador;
+    private EspacoPorto origem;
+    private EspacoPorto destino;
+    //pode ter 0 ou 1 espaconave
+    private boolean temEspaconave;
+    public Transporte(int identificador, EspacoPorto origem, EspacoPorto destino) {
+        this.identificador = identificador;
+        this.origem = origem;
+        this.destino = destino;
+    }
+
+    abstract double calculaDistancia();
+    abstract double calculaCusto();
 }
